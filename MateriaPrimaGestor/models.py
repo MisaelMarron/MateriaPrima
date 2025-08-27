bd2c65376b3a24ecc7cc838b689773a10a492334
joeCuadros <jcuadrosam@unsa.edu.pe>
2025-08-27 18:45:56 -0500

    feat: agregar modelos

--- MateriaPrimaGestor/models.py
+++ MateriaPrimaGestor/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class MateriaPrima(models.Model):
+    codigo = models.CharField(max_length=20, unique=True, primary_key=True)
+    nombre = models.CharField(max_length=100)
+    cantidad = models.DecimalField(max_digits=20, decimal_places=5)
+
+    def __str__(self):
+        return f"{self.codigo} - {self.nombre}"
+    
+class ProductoTerminado(models.Model):
+    codigo = models.CharField(max_length=20, unique=True, primary_key=True)
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.codigo} - {self.nombre}"
+    
+class DetalleProducto(models.Model):
+    codigoMateriaPrima = models.ForeignKey(MateriaPrima, on_delete=models.CASCADE)
+    codigoProductoTerminado = models.ForeignKey(ProductoTerminado, on_delete=models.CASCADE)
+    cantidad = models.DecimalField(max_digits=20, decimal_places=5)
+
+    class Meta:
+        unique_together = ("codigoMateriaPrima", "codigoProductoTerminado")
+
+    def __str__(self):
+        return f"{self.codigoProductoTerminado.nombre} usa {self.cantidad} Kg de {self.codigoMateriaPrima.nombre}"
