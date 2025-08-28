06a6ea25760ecf00236a1ee7d8d64867cd2d681e
joeCuadros <jcuadrosam@unsa.edu.pe>
2025-08-27 19:21:19 -0500

    feat: agregar register and login

--- MateriaPrimaGestor/views.py
+++ MateriaPrimaGestor/views.py
@@ -1,5 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 def inicio(request):
     return render(request, "inicio.html")
 
+
+# Registro
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Usuario creado correctamente ✅")
+            return redirect("inicio")
+        else:
+            messages.error(request, "Corrige los errores ❌")
+    else:
+        form = UserCreationForm()
+
+    return render(request, "register.html", {"form": form})
+# Login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()  # obtiene al usuario autenticado
+            login(request, user)
+            messages.success(request, f"Bienvenido {user.username} 👋")
+            return redirect("inicio")
+        else:
+            messages.error(request, "Credenciales inválidas ❌")
+    else:
+        form = AuthenticationForm()
+    
+    return render(request, "login.html", {"form": form})
+
+# Logout
+def logout_view(request):
+    logout(request)
+    messages.info(request, "Sesión cerrada correctamente 👋")
+    return redirect("inicio")
