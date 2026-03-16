59a70b80f6870a79e3c1b20f75c25cdd2de7318e
joeCuadros <jcuadrosam@unsa.edu.pe>
2026-03-15 22:20:36 -0500

    fear: 1 version de ejecutable servidor

--- MateriaPrima/settings.py
+++ MateriaPrima/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ixi3hhsw!7)m)k#zkvr7gq(w)uc2zna(&ufo#@m26bfnwow%q*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
