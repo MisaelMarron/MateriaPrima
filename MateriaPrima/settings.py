35f5c7aa064f168672921e1031d4d7ebd33ce9d1
joeCuadros <jcuadrosam@unsa.edu.pe>
2026-03-19 21:04:39 -0500

    fix: Css fallando por debug a false

--- MateriaPrima/settings.py
+++ MateriaPrima/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,7 +121,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     (os.path.join(BASE_DIR, 'MateriaPrimaGestor/static'))
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
